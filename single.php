e87c083fa02f44780903327c2ced99dc59d63b59
Muhammad Shafique <shofique32@gmail.com>
2021-10-20 18:23:11 +0600

    The code of single page has been updated.

--- single.php
+++ single.php
@@ -1,40 +1,14 @@
 <?php get_header(); ?>
 
 
-
-
-
-
-
-
-
-
 <?php 
     while (have_posts()) : the_post(); ?>
         <?php 
 
-            $file = get_field('file_upload');
-            if($file){
-                $link = $file["url"];
-            } 
+the_content(  );
+die();
+            
         ?>
-<div class="row">
-    <div class="col-sm-12">
-        <a href="<?php echo $link; ?>" download>Click here to download</a>
-    </div>
-</div>
-    <div class="row">
-        <div class="col">
-
-                <iframe src="<?php echo $link; ?>" width="100%" height="700px"></iframe>
-    <?php endwhile; ?>
-    </div>
-</div>
-    
-
-
-
-
-
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
