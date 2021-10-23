59a24bdcca93586468c77eb590f7c93a0eea9ff8
shafique-uddin <shofique32@gmail.com>
2021-10-23 23:18:41 +0600

    There have some problem in theme.

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ $text = '<div class="container-fluid">
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1 class="fs-3 text-center mb-4">দুঃখিত! <br> আপনার কাঙ্ক্ষিত তথ্য খুজে পাওয়া যায়নি।<br> নিশ্চয়ই আপনার ভুল হয়েছে।</h1>
+                <h1 class="fs-3 text-center mb-4">দুঃখিত! (404) <br> আপনার কাঙ্ক্ষিত তথ্য খুজে পাওয়া যায়নি।<br> নিশ্চয়ই আপনার ভুল হয়েছে।</h1>
             </div>
         </div>
     </div>
