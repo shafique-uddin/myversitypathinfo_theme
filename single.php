9cc6f19b970be2b0b9e6ba8cb753b36d24d6f998
shafique-uddin <shofique32@gmail.com>
2022-02-08 18:36:19 +0600

    single (post) page update

--- single.php
+++ single.php
@@ -3,12 +3,7 @@
 
 <?php 
     while (have_posts()) : the_post(); ?>
-        <?php 
-
-the_content(  );
-die();
-            
-        ?>
+        <?php the_content(  ); ?>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
