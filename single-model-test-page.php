1b9d470133a27a43a34d89bb382ebacbf6e79ee7
shafique-uddin <shofique32@gmail.com>
2023-03-27 17:53:57 +0600

    THEME UPDATED

--- single-model-test-page.php
+++ single-model-test-page.php
@@ -11,27 +11,46 @@ if((isset($_SESSION['usid']) && !empty($_SESSION['usuid']))){ ?>
 <!--MEMBER SECTION START-->
 <?php get_header('member'); ?>
 
+<div class="row">
+    <h2>Model Test : </h2>
+    
+</div>
 
 
+<form action="">
 <?php 
 $val1 = $_GET['modelTstNo'];
 
-
 $single_modlTst_result = apply_filters('single_model_test_info', "$val1");
 foreach ($single_modlTst_result as $key => $value) {
-    echo "<pre>";
-    print_r($value);
-    echo "</pre>";
-}
-
+    $options = explode("/",$value->question_options);
+    // echo "<pre>";
+    // print_r($options);
+    // echo "</pre>";
 ?>
 
-<div class="row">
-    <div class="question"><?php echo $value->question_title; ?></div>
-    <div class="options">
-        <input type="radio" name="" id="">
+    <div class="row member_question_section">
+        <div class="question">
+            <?php echo $value->question_title; ?>
+        </div> 
+        <div class="options">
+            <?php 
+                foreach ($options as $key => $Option_list) {
+                    if(strlen($Option_list > 0)){ ?>
+                        <input type="radio" name="<?php echo $value->id; ?>" id="<?php echo $Option_list; ?>" value="<?php echo $Option_list; ?>">
+                        <label for="<?php echo $Option_list; ?>"><?php echo $Option_list; ?></label><br>
+            <?php   }
+                }
+            ?>
+        </div>
     </div>
-</div>
+
+<?php } ?>
+
+    <div class="row">
+        <input type="submit" class="member-model-test-form-submit" value="Submit">
+    </div>
+</form>
 
 
 
